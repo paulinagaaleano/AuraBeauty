cf2f4802ce60bfd28551d8671a747c06bdf566e3
BrenEsq <brendaesquenazipersonal@gmail.com>
2026-08-31 19:19:24 -0300

    Ajuste de conexion BD local

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,21 @@ namespace AuraBeauty
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var con = Conexion.ObtenerConexion())
+                {
+                    con.Open();
+                    MessageBox.Show("¡Conexión a SQL Server exitosa!", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar con la base de datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
