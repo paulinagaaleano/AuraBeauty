260ec464b9ed084cf64109ee2ea4150cbd814bb2
Brenda Esquenazi <bresque12@gmail.com>
2026-08-31 22:45:15 -0300

    Base de datos en mi pc

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace AuraBeauty
+{
+    public class Conexion
+    {
+        // Ajustá 'Database=AuraBeautyDB' si le pusiste otro nombre a la base
+        private static readonly string Cadena =
+            @"Server=(localdb)\MSSQLLocalDB;Database=AuraBeautyDB;Integrated Security=True;TrustServerCertificate=True;";
+
+        public static SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(Cadena);
+        }
+    }
+}
